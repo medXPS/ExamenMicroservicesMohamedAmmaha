d6ef52a80fb5022d287a41ca9be8f3a2079326f2
medXPS <mohamedammaha2020@gmail.com>
2024-01-08 11:13:03 +0100

    adding latest version

--- API-Gateway/src/main/java/com/microservices/APIGateway/controller/FallBackController.java
+++ API-Gateway/src/main/java/com/microservices/APIGateway/controller/FallBackController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FallBackController {
-    @GetMapping("/svc1FallBack")
-   public String svc1FallBack(){
-        return" service 1 is currently down ";
+    @GetMapping("/studentFallBack")
+   public String studentFallBack(){
+        return" Student service  is currently down ";
     }
-    @GetMapping("/svc2FallBack")
-    public String svc2FallBack(){
-        return" service 2 is currently down ";
+    @GetMapping("/courseFallBack")
+    public String courseFallBack(){
+        return"Course  service  is currently down ";
     }
 
 }
